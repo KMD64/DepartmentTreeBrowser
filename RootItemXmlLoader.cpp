716bb17b835815f2cc730ed9960c6625424a455e
Maxwell <nintemod98@gmail.com>
2020-08-21 03:30:29 +0300

    Added saving to file

---
+++ RootItemXmlLoader.cpp
@@ -0,0 +1,6 @@
+#include "RootItemXmlLoader.h"
+
+RootItemXmlLoader::RootItemXmlLoader()
+{
+
+}
---
+++ RootItemXmlLoader.h
@@ -0,0 +1,11 @@
+#ifndef ROOTITEMXMLLOADER_H
+#define ROOTITEMXMLLOADER_H
+
+
+class RootItemXmlLoader
+{
+public:
+    RootItemXmlLoader();
+};
+
+#endif // ROOTITEMXMLLOADER_H
